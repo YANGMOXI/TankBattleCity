104ffc6bcdec7df6c19f36156734a6081a08940c
YANGMOXI <yangomoxi@163.com>
2021-02-28 10:57:31 +0800

    v1.14 优化子弹移动

--- tank_14.py
+++ tank_14.py
@@ -2,11 +2,11 @@
 # date: 2021/2/9 22:39
 
 """
-坦克大战 v1.13
+坦克大战 v1.14
 
-完善子弹的移动功能：
-	1.移动
-	2.碰到墙壁消失
+优化子弹的移动：
+	1.子弹打中墙壁时，直接消除
+	2.解决无限发射子弹问题（屏幕上最多出现3发子弹）
 """
 
 import pygame
@@ -16,7 +16,7 @@ import random
 _display = pygame.display
 COLOR_GRAY = pygame.Color(125, 125, 125)
 COLOR_BLACK = pygame.Color(0, 0, 0)
-VERSION = 'v1.13'
+VERSION = 'v1.14'
 P1_TANK_SIZE = pygame.image.load('img/p1tankU.gif').get_size()
 
 
@@ -67,7 +67,6 @@ class MainGame:
             # 子弹列表方法
             self.blitBullet()
 
-
             time.sleep(0.02)
             # 窗口刷新
             _display.update()
@@ -91,8 +90,11 @@ class MainGame:
     def blitBullet(self):
         """将子弹加入到窗口中"""
         for bullet in MainGame.Bullet_list:
-            bullet.displayBullet()
-            bullet.bulletMove()
+            if bullet.live:
+                bullet.displayBullet()
+                bullet.bulletMove()
+            else:
+                MainGame.Bullet_list.remove(bullet)
 
     def getEvent(self):
         """获取程序期间所有事件（鼠标事件、键盘事件）"""
@@ -126,10 +128,14 @@ class MainGame:
                     MainGame.TANK_P1.stop = False
                 elif event.key == pygame.K_SPACE:
                     print("发射子弹")
-                    # 产生一颗子弹
-                    m = Bullet(MainGame.TANK_P1)
-                    # 将子弹加入子弹列表
-                    MainGame.Bullet_list.append(m)
+                    if len(MainGame.Bullet_list) < 3:
+                        # 产生一颗子弹
+                        m = Bullet(MainGame.TANK_P1)
+                        # 将子弹加入子弹列表
+                        MainGame.Bullet_list.append(m)
+                    else:
+                        print("子弹数量不足")
+                    print("当前子弹数量:%d" %len(MainGame.Bullet_list))
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
@@ -169,7 +175,7 @@ class Tank:
         # 指定初始化位置
         self.rect.left = left
         self.rect.top = top
-        self.speed = 5  # 速度/单位位移
+        self.speed = 8  # 速度/单位位移
         self.stop = True  # 坦克移动开关
 
     def move(self):
@@ -280,21 +286,31 @@ class Bullet:
         elif self.direction == 'R':
             self.rect.left = tank.rect.left + tank.rect.width
             self.rect.top = tank.rect.top + tank.rect.height / 2 - self.rect.width / 2
+        # 标签 - 记录子弹是否撞到墙壁
+        self.live = True
 
     def bulletMove(self):
         """子弹移动"""
         if self.direction == 'U':
             if self.rect.top > 0:
                 self.rect.top -= self.speed
+            else: # 撞墙消失
+                self.live = False
         elif self.direction == 'D':
             if self.rect.top < MainGame.SCREEN_HIGHT - self.rect.height:
                 self.rect.top += self.speed
+            else:
+                self.live = False
         elif self.direction == 'L':
             if self.rect.left > 0:
                 self.rect.left -= self.speed
+            else:
+                self.live = False
         elif self.direction == 'R':
             if self.rect.left < MainGame.SCREEN_WIDTH - self.rect.width:
                 self.rect.left += self.speed
+            else:
+                self.live = False
 
     def displayBullet(self):
         """展示子弹 - 在窗口中显示"""
