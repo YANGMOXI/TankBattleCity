d8c2f65b00b480f9fad8c5d2ea97ed83ae2e5fd2
YANGMOXI <yangomoxi@163.com>
2021-02-28 10:41:12 +0800

    v1.03 事件控制：方向控制、子弹发射

--- tank_03.py
+++ tank_03.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
-# date: 2021/2/9 21:26
+# date: 2021/2/9 22:39
 
 """
-坦克大战 v1.02
+坦克大战 v1.03
 
 新增功能：
-    创建游戏窗口 —— pygame中模块使用
+    事件处理：
+        点击关闭按钮，退出程序的事件
+        方向控制
+        子弹发射
 """
+
 
 import pygame
 
 _display = pygame.display
 COLOR_GRAY = pygame.Color(125,125,125)
+VERSION = 'v1.03'
 
 class MainGame:
     """主逻辑"""
@@ -28,11 +33,35 @@ class MainGame:
         # 创建窗口，加载窗口 -> surface
         MainGame.window = _display.set_mode(size=(MainGame.SCREEN_WIDTH, MainGame.SCREEN_HIGHT))
         # 设置游戏标题
-        _display.set_caption('坦克大战v1.02')
+        _display.set_caption('坦克大战%s' %VERSION)
         # 让窗口持续刷新操作
         while True:
-            MainGame.window.fill(MainGame.COLOR_GRAY) # 给窗口 纯色填充
+            MainGame.window.fill(COLOR_GRAY)  # 给窗口 纯色填充
+            self.getEvent()  # 持续完成事件的获取
             _display.update()
+
+    def getEvent(self):
+        """获取程序期间所有事件（鼠标事件、键盘事件）"""
+        # 获取所有事件
+        # 对事件判断处理1.鼠标事件
+        eventList = pygame.event.get()
+
+        for event in eventList:
+            # 点击关闭按钮（鼠标事件）
+            if event.type == pygame.QUIT:
+                self.endGame()
+            # 按键判断
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    print("坦克向左")
+                elif event.key == pygame.K_RIGHT:
+                    print("坦克向右")
+                elif event.key == pygame.K_UP:
+                    print("坦克向上")
+                elif event.key == pygame.K_DOWN:
+                    print("坦克向下")
+                elif event.key == pygame.K_SPACE:
+                    print("发射子弹")
 
 
     def endGame(self):
@@ -116,4 +145,5 @@ class Music:
 
 
 if __name__ == '__main__':
-    MainGame().startGame()
+    game1 = MainGame()
+    game1.startGame()
